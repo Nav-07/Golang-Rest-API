197c36d610c826102d8b75fb794e59d7f1b870a6
= <navneil2006@gmail.com>
2020-06-09 18:34:41 +1000

    Laid the Basic Structure for the Project

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
 )
 
+type User struct {
+	name string `json:"name"`
+	mail string `json:"mail"`
+	phone string `json:"phone"`
+}
+
 func main() {
-	fmt.Println("Hello, World")
+	// Init Router
+	router := max.NewRouter()
+
+	// Create the Endpoints
+	router.HandleFunc("/api/users", createUser, "POST")
+	router.HandleFunc("/api/users/{id}", getUser, "GET")
+	router.HandleFunc("/api/users", getUsers, "GET")
+
+	// Launch the Server
+	log.Fatal(http.ListenAndServe("0.0.0.0:50", router))
 }
