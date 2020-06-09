5a353400671f3d1dd91f8dcad17dd2726a95e170
= <navneil2006@gmail.com>
2020-06-09 18:59:22 +1000

    Get Users Request Done

--- main.go
+++ main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
 	"log"
 	"net/http"
-	"math/rand"
-	"strconv"
 	"github.com/gorilla/mux"
+	"encoding/json"
 )
 
 type User struct {
-	name string `json:"name"`
-	mail string `json:"mail"`
-	phone string `json:"phone"`
+	Name string `json:"name"`
+	Mail string `json:"mail"`
+	Phone string `json:"phone"`
+}
+
+var users []User
+
+func createUser(h http.ResponseWriter, r *http.Request) {
+}
+func getUser(h http.ResponseWriter, r *http.Request) {
+}
+func getUsers(h http.ResponseWriter, r *http.Request) {
+	h.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(h).Encode(users)
 }
 
 func main() {
 	// Init Router
-	router := max.NewRouter()
+	router := mux.NewRouter()
+
+	// Enter Mock Data
+	users = append(users, User{Name: "Nav Pahwa", Mail: "donkey@gmail.com", Phone: "+1000000000"})
+	users = append(users, User{Name: "John Doe", Mail: "john.doe@gmail.com", Phone: "+1000654000"})
+	users = append(users, User{Name: "Sam Smith", Mail: "sam.smith@gmail.com", Phone: "+1007830000"})
 
 	// Create the Endpoints
-	router.HandleFunc("/api/users", createUser, "POST")
-	router.HandleFunc("/api/users/{id}", getUser, "GET")
-	router.HandleFunc("/api/users", getUsers, "GET")
+	router.HandleFunc("/api/users", createUser).Methods("POST")
+	router.HandleFunc("/api/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/api/users", getUsers).Methods("GET")
 
 	// Launch the Server
 	log.Fatal(http.ListenAndServe("0.0.0.0:50", router))
